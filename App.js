38ce13723dcc76109702fdc3bc103e5a12d6e29f
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2020-12-27 12:25:13 -0300

    estrutura formada

--- App.js
+++ App.js
@@ -1,14 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component }from 'react';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      tempo: '0.0s',
+      zerar: 'Zerar',
+      comecar: 'Vai!',
+      contando: true,
+    }
+    this.contador = this.contador.bind(this)
+  }
+
+  contador = () => {
+
+    while(this.state.contando){
+      alert('oi')
+      break
+    }
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}>Logo</Text>
+        <Text style={styles.time}>{this.state.tempo}</Text>
+        <View style={styles.buttons}>
+          <TouchableOpacity style={styles.vai} onPress={this.contador}>
+            <Text style={styles.textVai}>{this.state.comecar}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.zerar}>
+            <Text style={styles.textZerar}>{this.state.zerar}</Text>
+          </TouchableOpacity>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +50,46 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontFamily: 'Arial'
   },
+  logo:{
+    position: 'absolute',
+    backgroundColor: 'lightblue',
+    fontSize: 40,
+    padding: 30,
+    top: 100,
+  },
+  time:{
+    fontSize: 30,
+
+  },
+  buttons:{
+    width: '95%',
+    top: 30,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  vai:{
+    backgroundColor: 'pink',
+    width: 150,
+    height: 50,
+    justifyContent: 'center',
+    borderRadius: 15,
+  },
+  textVai:{
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  zerar:{
+    backgroundColor: 'green',
+    width: 150,
+    height: 50,
+    justifyContent: 'center',
+    borderRadius: 15,
+  },
+  textZerar:{
+    textAlign: 'center',
+    fontSize: 20,
+  }
 });
